e267d92223ed7bdf910006a6e2b8d090d06c40cd
NepMouse <250776924@qq.com>
2019-04-28 14:53:11 +0800

    get_time_function

--- C_Module/time.c
+++ C_Module/time.c
@@ -2,13 +2,21 @@
 #include <unistd.h>
 #include <stdlib.h>
 #include <time.h>
- 
-int main(void)
+
+void get_time(struct tm **lt);
+
+void get_time(struct tm **lt)
 {
     time_t t;
-    struct tm * lt;
     time (&t);//获取Unix时间戳。
-    lt = localtime (&t);//转为时间结构。
+    *lt = localtime (&t);//转为时间结构。
+    return;
+}
+
+int main(int argc, char **argv)
+{
+    struct tm *lt;
+    get_time(&lt);//获取时间。
     printf ( "%d/%d/%d %d:%d:%d \n",lt->tm_year+1900, lt->tm_mon, lt->tm_mday, lt->tm_hour, lt->tm_min, lt->tm_sec);//输出结果
     return 0;
 }
